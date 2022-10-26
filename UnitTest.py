21079aea66bcd9ea446d99b0a69d123db240d68e
Oleksandr Petrushchak <60898768+petrushchako@users.noreply.github.com>
2022-10-26 22:02:16 +0100

    Create UnitTest.py

---
+++ UnitTest.py
@@ -0,0 +1,9 @@
+import unittest
+
+class Testing(unittest.TestCase):
+    def test_string(self):
+        x,y = "Alpha","Alpha"
+        self.assertEqual(x,y)
+
+if __name__=="__main__":
+    unittest.main()
