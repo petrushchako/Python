d237ebbd23a7d7732cf83f3317051191cf48f34e
Alex Petrushchak <petrushchako@gmail.com>
2023-10-16 23:38:22 +0100

    Add content

--- JSON/workingWithJSON.py
+++ JSON/workingWithJSON.py
@@ -22,8 +22,16 @@ people_string = '''
 data = json.loads(people_string)
 print(type(data))
 print(data)
-print(data["people"][1]["name"])
+#print(data["people"][1]["name"])
+#Itareta through objects in people list
+for person in data["people"]:
+    for key, value in person.items():
+        print("{} : {}".format(key,value))
 
+#Access names only
+print("\nAccess names only:")
+for person in data["people"]:
+    print("\t",person["name"])
 
 
 with open("jsonFiles/sample.json", 'r') as file:
@@ -32,3 +40,14 @@ with open("jsonFiles/sample.json", 'r') as file:
 
 print("\n"*3,type(sampleJSON))
 print(sampleJSON)
+
+#modify JSON
+sampleJSON['grades']['math'] = 85
+print(sampleJSON)
+with open("jsonFiles/sample.json", "w") as file:
+    json.dump(sampleJSON, file, indent=4)
+
+#Dumps example
+print("\njson.dumps(indent=4, sort_keys=True) example:")
+jsonDumpsExample = json.dumps(sampleJSON, indent=4, sort_keys=True)
+print(jsonDumpsExample)
