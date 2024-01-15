417df4e489a7f846d1b40c578aee88cefe3ed016
Alex Petrushchak <petrushchako@gmail.com>
2024-01-15 21:48:19 +0000

    Add List methods

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
-def printText():
-    pass
-    print("I'm printing from script")
 
-if __name__=="__main__":
-    printText()
+mixed_list = ["Nissan Sentra", 1, 2.5, True]
+
+for i in mixed_list:
+    print(type(i))
+
+mixed_list[0] = "Ford Mondeo"
+mixed_list.insert(7, "Audi")
+mixed_list.insert(6, "BMW")
+
+print(mixed_list)
