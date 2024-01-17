4e0ad91fa9af48a183e6dac6c9ef33892c0c4fc7
Alex Petrushchak <petrushchako@gmail.com>
2024-01-17 20:35:22 +0000

    Add List built-in methods

--- test.py
+++ test.py
@@ -5,3 +5,25 @@ list3 = copy.copy(list1)
 list3[1][1]='x'
 
 print("List 1 : ",list1, "\nList 2 : ", list2, "\nList 3 : ", list3)
+
+
+print("\n"*3)
+
+list_num = [20, 70, 80, 50, 30, 60, 40]
+
+print(max(list_num))
+print(min(list_num))
+print(len(list_num))
+
+list_num.sort() #Sort the origin list
+print(sorted(list_num))
+
+list_num.append(25)
+
+print(list_num)
+
+list_num_sorted = sorted(list_num, reverse=True)
+
+print(list_num_sorted)
+
+print(all([]))
