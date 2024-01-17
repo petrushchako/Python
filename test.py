b3fbd6c84d4c3f42b1f7a4e512abb638ea502b43
Alex Petrushchak <petrushchako@gmail.com>
2024-01-17 20:58:54 +0000

    Add list slicing

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ list3[1][1]='x'
 print("List 1 : ",list1, "\nList 2 : ", list2, "\nList 3 : ", list3)
 
 
-print("\n"*3)
+print("-"*30)
 
 list_num = [20, 70, 80, 50, 30, 60, 40]
 
@@ -27,3 +27,17 @@ list_num_sorted = sorted(list_num, reverse=True)
 print(list_num_sorted)
 
 print(all([]))
+
+
+
+print("-"*30)
+cars= ["Audi", "BMW", "Mercedes-Benz", "Volkswagen", "Volvo"]
+print(cars)
+len(cars) # 4
+cars[4] # Volvo
+sliced_cars = cars[0:7] # ["Audi", "BMW", "Mercedes-Benz", "Volkswagen", "Volvo"]
+print(sliced_cars)
+sliced_cars = cars[0:-1:2] # ['Audi', 'Mercedes-Benz']
+sliced_cars= cars[0::2] # ['Audi', 'Mercedes-Benz', 'Volvo']
+reversed_cars=cars[6::-1]
+print(reversed_cars)
