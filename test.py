98b0f5cf21312954f7782c1f02c16dec91daac9b
Alex Petrushchak <petrushchako@gmail.com>
2024-01-26 00:34:23 +0000

    Add exception payload content

--- test.py
+++ test.py
@@ -347,3 +347,35 @@ finally:
     print(f"File closed : {file.closed}")
 
 
+import errno, os
+
+for i in dir(errno):
+    print(i)
+def check_file(filepath):
+    if not os.path.isfile(filepath):
+        raise FileNotFoundError # Not recommended
+
+try:
+    check_file('invalid.path')
+except FileNotFoundError as exception:
+    print('#1 Handled FileNotFoundError:', exception)
+
+def check_file_message(filepath):
+    if not os.path.isfile(filepath):
+            raise FileNotFoundError('"%s" not found' % filepath)
+
+try:
+    check_file_message('invalid.path')
+except FileNotFoundError as exception:
+    print('#2 Handled FileNotFoundError:', exception)
+
+def check_file_args(filename) :
+    if not os.path.isfile(filename):
+        raise FileNotFoundError(errno.ENOENT, os. strerror (errno. ENOENT), 
+                                filename, 2, 'filename2')
+
+try:
+    check_file_args('invalid.path')
+except FileNotFoundError as exception:
+    print('#3 Handled FileNotFoundError:', exception)
+    print('Arguments of exception:', exception. args)
