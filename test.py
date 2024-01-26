39df9709464ba248259736464ba2ed4cccdccc1c
Alex Petrushchak <petrushchako@gmail.com>
2024-01-26 00:06:42 +0000

    Raise the exception

--- test.py
+++ test.py
@@ -347,27 +347,3 @@ finally:
     print(f"File closed : {file.closed}")
 
 
-
-try:
-    file=open("testFile-TOBEDELETED.txt", "w")
-    file.write("Writing to the open file")
-except:
-    print("Oops! Something went wrong")
-else:
-    print("Nothing went wrong")
-
-
-import os
-
-file_path = 'testFile-TOBEDELETED.txt'
-
-try:
-    os.remove(file_path)
-    print(f"File '{file_path}' deleted successfully.")
-except FileNotFoundError:
-    print(f"File '{file_path}' not found.")
-except PermissionError:
-    print(f"Permission error: Unable to delete '{file_path}'.")
-except Exception as e:
-    print(f"An error occurred: {str(e)}")
-
