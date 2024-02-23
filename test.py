b5718750290f8314af2c7867b73995767e4f28f4
Oleksandr Petrushchak <60898768+petrushchako@users.noreply.github.com>
2024-02-23 23:31:31 +0000

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ list1 = [1, [2, 3], 4]
 list2 = copy.deepcopy(list1)
 list3 = copy.copy(list1)
 list3[1][1]='x'
-
 print("List 1 : ",list1, "\nList 2 : ", list2, "\nList 3 : ", list3)
+
 
 
 print("-"*30)
@@ -19,15 +19,12 @@ list_num.sort() #Sort the origin list
 print(sorted(list_num))
 
 list_num.append(25)
-
 print(list_num)
 
 list_num_sorted = sorted(list_num, reverse=True)
 
 print(list_num_sorted)
-
 print(all([]))
-
 
 
 print("-"*30)
