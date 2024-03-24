53d7cad7174b28632e51476f4ec19cc21330d259
Alex Petrushchak <petrushchako@gmail.com>
2024-03-24 22:05:11 +0000

    Add lambda examples

--- PCAP-31-03/Module_4_Miscellaneous/Lambdas.py
+++ PCAP-31-03/Module_4_Miscellaneous/Lambdas.py
@@ -49,3 +49,26 @@ print("(lambda x : x*10)(33) : \t",(lambda x : x*10)(33))
 print("(lambda x,y: x*y)(33, 10) : \t",(lambda x,y: x*y)(33, 10))
 
 print("-"* 60,"\n    \n")
+
+create_student_record_fn1 = \
+    lambda name, major, university : \
+        {'name': name, 'major': major, 'university': university}
+
+print(create_student_record_fn1("Alex", "Cloud Computing", "ATU"))
+
+
+create_student_record_fn2 = \
+    lambda name , major = "Arts", university = "NUIG": \
+        {'name': name, 'major': major, 'university': university}
+
+print(create_student_record_fn2("Kristina"))
+
+
+create_student_list_fn = lambda *args : [name for name in args]
+print("create_student_list_fn(\"Alex\", \"Bob\", \"Charlie\") : ", create_student_list_fn("Alex", "Bob", "Charlie"))
+
+create_student_record_fn = lambda **kwargs : {key:value for key,value in kwargs.items()}
+value1 = create_student_record_fn(name = "Svitlana", year = 1970)
+print(type(value1), " : ", value1)
+value2 = create_student_record_fn(name = "Volodimir", year = 1970, location = "Poland")
+print(type(value2), " : ", value2)
