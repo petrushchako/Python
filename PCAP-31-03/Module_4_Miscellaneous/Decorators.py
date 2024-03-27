7b7f6241cf6c390e516cb187c59539cab061a86c
Oleksandr Petrushchak <60898768+petrushchako@users.noreply.github.com>
2024-03-27 14:24:11 +0000

    Add Decorator example

---
+++ PCAP-31-03/Module_4_Miscellaneous/Decorators.py
@@ -0,0 +1,16 @@
+# Modifying code using Python Decorators
+
+def non_negative_arguments(decorated_fn):
+    def check_non_negative(*args):
+        for arg in args:
+            if arg <= 0:
+                raise ValueError("Argument cannot be negative or zero")
+        return decorated_fn(*args)
+    return check_non_negative
+
+@non_negative_arguments
+def compute_rectangle_area(length, breadth):
+    return length * breadth
+
+print(compute_rectangle_area(3,4))
+print(compute_rectangle_area(-3,4)) # Will raise ValueError from line 7
