e473e34a333d3016b658756033f8775f77ae3b48
Alex Petrushchak <petrushchako@gmail.com>
2024-03-28 23:31:44 +0000

    Add list comprehension examples

--- PCAP-31-03/Module_4_Miscellaneous/ListComprehension.py
+++ PCAP-31-03/Module_4_Miscellaneous/ListComprehension.py
@@ -11,3 +11,11 @@ print(numbers_copy)
 
 comprehension_copy = [num for num in numbers]
 print(comprehension_copy)
+
+# Enhanced comprehension example
+numbers = [1,2,3,4,5,6]
+odd_numbers = [num for num in numbers if (lambda x: x%2!=0)(num)]
+even_numbers = [num for num in numbers if num%2==0]
+
+print("Odd numbers (with lambda check): ", odd_numbers)
+print("Even numbers:", even_numbers)
