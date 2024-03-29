d12ce5848e8ec6e241e0e34f26e97e4b2874d49a
Alex Petrushchak <petrushchako@gmail.com>
2024-03-29 21:26:33 +0000

    Add alphabet examples

--- PCAP-31-03/Module_4_Miscellaneous/ListComprehension.py
+++ PCAP-31-03/Module_4_Miscellaneous/ListComprehension.py
@@ -29,3 +29,34 @@ print("square_num2: ",square_num2)
 words_list = ["Hi", "there", "how", "are", "you"]
 upper_words_list = [word.upper()[:2] for word in words_list]
 print(f"{words_list} \n{upper_words_list}")
+
+
+letters = [x for x in "Harry Potter" if x.upper() in ['A', 'O', 'E', 'I', 'U']]
+print(''.join(letters))
+
+
+#########################################################################
+import string
+
+print("\n\nAlphabet examples")
+
+lowercase_letters = list(string.ascii_lowercase)
+print(lowercase_letters)
+vowels = ['a','e','i','o','u']
+consonant_letters = [x for x in lowercase_letters if x not in vowels]
+
+print("\n\nConsonant Letters:")
+for i, letter in enumerate(consonant_letters):
+    if i % 5 == 0 and i != 0:
+        print()
+    print(letter, end=" ")
+
+
+# Filter example    
+filtered_alphabet = list(filter(lambda char: char not in vowels, lowercase_letters))
+print("\n\nFiltered Alphabet:")
+
+for i, letter in enumerate(filtered_alphabet):
+    if i%5 == 0 and i != 0:
+        print()
+    print(letter, end = " ")
