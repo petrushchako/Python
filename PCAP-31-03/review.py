6c87a692ed84515d55d788097c9e76ed72faba19
Alex Petrushchak <petrushchako@gmail.com>
2024-04-11 22:44:43 +0100

    Add collection functions examples

--- PCAP-31-03/review.py
+++ PCAP-31-03/review.py
@@ -10,3 +10,25 @@ add_lambda = lambda a,b: a+b
 assert add_func(2,3)== add_lambda(2,3)
 
 print("Lambda example :" , add_lambda(2,3))
+
+
+##############################################
+# Collection functions
+##############################################
+domain = [1,2,3,4,5,6]
+
+square_vals = list(map(lambda x: x**2, domain))
+print("map() example:", square_vals)
+
+evens = list(filter(lambda x: x%2==0, domain))
+print("filter() example:", evens)
+
+from functools import reduce
+sum_of_values = reduce(lambda acc, num: acc+num, domain, 0)
+print("Sum of numbers:", sum_of_values)
+
+sentence = ["Boss", "a", "Alfred", "fig", "Daemon", "dig"]
+print("\n\nSorting by default")
+print(f"Original list:\n{sentence}")
+print(f"sorted() default:\n{sorted(sentence)}")
+print(f"sorted() with key\n{sorted(sentence, key=lambda x: x.lower())}")
