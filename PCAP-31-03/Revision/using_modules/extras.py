c85f9d8012b822c8deed829cca45004404d36fa9
Alex Petrushchak <petrushchako@gmail.com>
2024-04-14 00:14:29 +0100

    Add hidding module entities example

---
+++ PCAP-31-03/Revision/using_modules/extras.py
@@ -0,0 +1,9 @@
+print(f"\t__name__ in extras.py:\t{__name__}")
+print("Importing 'helpers' in 'extras'")
+import helpers
+
+name = "Keith Thompson"
+
+
+#from helpers import * #import everything from __all__ reference in helpers.py module 
+#print(extract_lower("New York"))
--- PCAP-31-03/Revision/using_modules/main.py
+++ PCAP-31-03/Revision/using_modules/main.py
@@ -1,5 +1,10 @@
+print(f"\t__name__ in main.py:\t{__name__}")
+print("Importing 'helpers' from 'main'")
 import helpers
 from helpers import extract_lower as e_low, extract_upper
+
+
+
 
 hw = ["HELLO", "world"]
 
@@ -8,8 +13,8 @@ print(helpers.extract_upper(hw))
 print(helpers.extract_lower(hw))
 
 
+print("Importing 'extras' from 'main'")
+import extras
 
-name = "Keith Thomson"
-
-print(f"Uppercase letter: {extract_upper(name)}")
-print(f"Lowercase letter: {e_low(name)}")
+print(f"Uppercase letter: {extract_upper(extras.name)}")
+print(f"Lowercase letter: {e_low(extras.name)}")
