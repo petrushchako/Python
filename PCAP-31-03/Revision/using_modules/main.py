d83aa8fd54fedaa98d8e74a634825288dc9d78db
Alex Petrushchak <petrushchako@gmail.com>
2024-04-13 23:06:57 +0100

    Add module usecase

---
+++ PCAP-31-03/Revision/using_modules/main.py
@@ -0,0 +1,14 @@
+import helpers
+
+hw = ["HELLO", "world"]
+
+
+print(helpers.extract_upper(hw))
+print(helpers.extract_lower(hw))
+
+
+
+name = "Keith Thomson"
+
+print(f"Uppercase letter: {helpers.extract_upper(name)}")
+print(f"Lowercase letter: {helpers.extract_lower(name)}")
