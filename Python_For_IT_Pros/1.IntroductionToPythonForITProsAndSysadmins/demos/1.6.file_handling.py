f6fd9dba274b72f2a4d7ae7b47e581426ab0b667
Oleksandr Petrushchak <petrushchako@gmail.com>
2025-08-22 22:56:33 +0100

    Add writing to a file example

--- Python_For_IT_Pros/1.IntroductionToPythonForITProsAndSysadmins/demos/1.6.file_handling.py
+++ Python_For_IT_Pros/1.IntroductionToPythonForITProsAndSysadmins/demos/1.6.file_handling.py
@@ -1,4 +1,19 @@
+# Output example
+output_path = "output.txt"
+message = "This is my my message in the bottle"
+
+try:
+    with open(output_path, "w") as file:
+        file.write(message)
+    print(f"Data witten to a {output_path}")
+except Exception as e:
+    print("Error: ", e)
+
+
+# Reading a file
+# If file does not exist r/r+ the FileNotFoundError exception will be rissen
 file_path = "example.txt"
+
 try:
     with open(file_path, "r") as file:
         content = file.read()
@@ -6,3 +21,5 @@ try:
         print(content)
 except FileNotFoundError as e:
     print("Error: ", e)
+finally:
+    print("Exiting application")
