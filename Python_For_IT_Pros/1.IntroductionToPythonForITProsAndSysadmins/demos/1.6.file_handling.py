acc1e38e2e89c3c26f6206aca95f724cb7e4547d
Oleksandr Petrushchak <petrushchako@gmail.com>
2025-08-23 22:09:03 +0100

    Add file pointer manipulation content

--- Python_For_IT_Pros/1.IntroductionToPythonForITProsAndSysadmins/demos/1.6.file_handling.py
+++ Python_For_IT_Pros/1.IntroductionToPythonForITProsAndSysadmins/demos/1.6.file_handling.py
@@ -23,3 +23,33 @@ except FileNotFoundError as e:
     print("Error: ", e)
 finally:
     print("Exiting application")
+
+
+def openFileA():
+    ' ' ' Pointer manipulation ' ' '
+    # Opening file in 'a' mode, sets pointer to the end of the file
+    with open(output_path, "a+") as file:
+        position = file.tell()
+        print(f"Content of the file (starting at position {position}):\n{file.read()}") 
+        file.seek(0)
+        position = file.tell()
+        print(f"Content of the file (starting at position {position}):\n{file.read()}")
+
+def openFileRB():
+    """        
+    seek(offset, whence)
+    offset → number of bytes to move
+    whence → reference point:
+        0 = from start of file (default)
+        1 = from current position
+        2 = from end of file
+    """
+    with open(output_path, "rb") as file:
+        
+        # Switch poisiton of the point to 7 characters from the end and read the message
+        file.seek(-7, 2)
+        print(file.read())
+
+
+print(openFileRB.__doc__)
+print(openFileA.__doc__)
