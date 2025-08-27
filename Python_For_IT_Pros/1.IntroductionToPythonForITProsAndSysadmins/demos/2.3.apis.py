ead03005ded176a9435a55ec7e99a6056497568b
Oleksandr Petrushchak <petrushchako@gmail.com>
2025-08-27 14:10:22 +0100

    Add api demo PEP-8 changes

--- Python_For_IT_Pros/1.IntroductionToPythonForITProsAndSysadmins/demos/2.3.apis.py
+++ Python_For_IT_Pros/1.IntroductionToPythonForITProsAndSysadmins/demos/2.3.apis.py
@@ -2,20 +2,32 @@ import requests
 
 api_url = "https://api.github.com"
 username = "petrushchako" # input("Enter Github username: ")
-
 headers = {
     "Accept" : "application/vnd.github.v3+json",
     "User-Agent" : ""
 }
-
 repos_url = f"{api_url}/users/{username}/repos"
-r = requests.get(url=repos_url, headers=headers)
 
-if r.status_code == 200:
-    repositories = r.json()
-    print(f"Repos for {username}:")
-    with open("response.json", "w")as file:
-        file.write(str(repositories[0]))
-    #print(len(repositories))
-    for repo in repositories:
-        print(f"{"-"*100}\n[{repo["name"]}]\n\tDescription: {repo["description"]}\n\tURL: {repo["owner"]["repos_url"]}\n{"-"*100}")
+
+def getRepos(output):
+    r = requests.get(url=repos_url, headers=headers)
+    if r.status_code == 200:
+        repositories = r.json()
+        print(f"Repos for {username}:")
+
+        if output:
+            with open("response.json", "w")as file:
+                file.write(str(repositories[0]))
+
+        for repo in repositories:
+            print(
+                f"{'-'*100}\n"
+                f"[{repo['name']}]\n"
+                f"\tDescription: {repo['description']}\n"
+                f"\tURL: {repo['owner']['repos_url']}\n"
+                f"{'-'*100}"
+            )
+
+
+if __name__ == "__main__":
+    getRepos(output=False)
