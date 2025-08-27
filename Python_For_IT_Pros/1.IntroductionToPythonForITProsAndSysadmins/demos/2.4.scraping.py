5104f1d4876e0fc071df138648e24e122b44b512
Oleksandr Petrushchak <petrushchako@gmail.com>
2025-08-27 20:30:22 +0100

    Add web scraping code

---
+++ Python_For_IT_Pros/1.IntroductionToPythonForITProsAndSysadmins/demos/2.4.scraping.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+"""
+BeautifulSoup4 documentation https://www.crummy.com/software/BeautifulSoup/bs4/doc/#
+"""
+
+url = "https://www.crummy.com/software/BeautifulSoup/"
+
+r = requests.get(url)
+
+if r.status_code == 200:
+    soup = BeautifulSoup(r.content, "html.parser")
+    headers = soup.find_all("a")
+
+    for header in headers:
+        print(header.get("href"))
+else:
+    print(f"Response status: {r.status_code}")
