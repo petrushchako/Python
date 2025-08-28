5bd5b3d8fc3097dd827944861b16285619f6dd88
Alex Petrushchak <petrushchako@gmail.com>
2025-08-28 20:34:40 +0100

    Add formatting fixes

--- PCAP-31-03/test.py
+++ PCAP-31-03/test.py
@@ -1,13 +1,13 @@
 import copy
+
 list1 = [1, [2, 3], 4]
 list2 = copy.deepcopy(list1)
 list3 = copy.copy(list1)
-list3[1][1]='x'
-print("List 1 : ",list1, "\nList 2 : ", list2, "\nList 3 : ", list3)
+list3[1][1] = "x"
+print("List 1 : ", list1, "\nList 2 : ", list2, "\nList 3 : ", list3)
 
 
-
-print("-"*30)
+print("-" * 30)
 
 list_num = [20, 70, 80, 50, 30, 60, 40]
 
@@ -15,7 +15,7 @@ print(max(list_num))
 print(min(list_num))
 print(len(list_num))
 
-list_num.sort() #Sort the origin list
+list_num.sort()  # Sort the origin list
 print(sorted(list_num))
 
 list_num.append(25)
@@ -27,21 +27,31 @@ print(list_num_sorted)
 print(all([]))
 
 
-print("-"*30)
-cars= ["Audi", "BMW", "Mercedes-Benz", "Volkswagen", "Volvo"]
+print("-" * 30)
+cars = ["Audi", "BMW", "Mercedes-Benz", "Volkswagen", "Volvo"]
 print(cars)
-len(cars) # 4
-cars[4] # Volvo
-sliced_cars = cars[0:7] # ["Audi", "BMW", "Mercedes-Benz", "Volkswagen", "Volvo"]
+len(cars)  # 4
+cars[4]  # Volvo
+sliced_cars = cars[0:7]  # ["Audi", "BMW", "Mercedes-Benz", "Volkswagen", "Volvo"]
 print(sliced_cars)
-sliced_cars = cars[0:-1:2] # ['Audi', 'Mercedes-Benz']
-sliced_cars= cars[0::2] # ['Audi', 'Mercedes-Benz', 'Volvo']
-reversed_cars=cars[6::-1]
+sliced_cars = cars[0:-1:2]  # ['Audi', 'Mercedes-Benz']
+sliced_cars = cars[0::2]  # ['Audi', 'Mercedes-Benz', 'Volvo']
+reversed_cars = cars[6::-1]
 print(reversed_cars)
 
 
-my_car_list = ['Toyota Corolla', 'Toyota Camry', 'Nissan Altima', 'Kia Soul', 'Kia Optima', 'Honda Civic']
-my_car_list[6:-1] = ['Ford Focus', 'Dodge Charger'] #Assign values to indexes 6 and 7/-1
+my_car_list = [
+    "Toyota Corolla",
+    "Toyota Camry",
+    "Nissan Altima",
+    "Kia Soul",
+    "Kia Optima",
+    "Honda Civic",
+]
+my_car_list[6:-1] = [
+    "Ford Focus",
+    "Dodge Charger",
+]  # Assign values to indexes 6 and 7/-1
 print(my_car_list)
 print(my_car_list[::-1])
 
@@ -54,31 +64,30 @@ print("Ford Focus" in my_car_list)
 x = "World"
 print(x[::2])
 
-#x[0]="G"
+# x[0]="G"
 
 
 x = "Hello"
-print(x.startswith("H")) #True
-print(x.startswith("h")) #False
+print(x.startswith("H"))  # True
+print(x.startswith("h"))  # False
 
-print(x.endswith("o")) #True
-print(x.endswith("O")) #False
+print(x.endswith("o"))  # True
+print(x.endswith("O"))  # False
 
-print(x.count('l'))
-print("-"*30)
-print(x.count('l')) # 2
-print(x.find("H")) # 0
-print(x.find("h")) # -1
+print(x.count("l"))
+print("-" * 30)
+print(x.count("l"))  # 2
+print(x.find("H"))  # 0
+print(x.find("h"))  # -1
 print(x.index("H"))
-#print(x.index("h")) # ValueError: substring not found
-
+# print(x.index("h")) # ValueError: substring not found
 
 
 x = "New York City"
 y = []
 z = ""
 
-if(x.count(" ")> 0):
+if x.count(" ") > 0:
     y = x.split(" ")
     z = " ".join(y)
     print(y, "\n", z)
@@ -87,9 +96,9 @@ if(x.count(" ")> 0):
 x1 = x[0:3]
 print(x[1:3].islower())
 
-print("-"*10, "TUPLES", "-"*10)
+print("-" * 10, "TUPLES", "-" * 10)
 my_tuple = ()
-print(my_tuple) # <class 'tuple'>
+print(my_tuple)  # <class 'tuple'>
 
 int_tuple = (1, 2, 3)
 str_tuple = ("Hello", "Python")
@@ -99,14 +108,14 @@ combinet_tuple = int_tuple + str_tuple
 print(combinet_tuple * 2)
 
 tuple1 = 1, "Hello", 3.14
-print(tuple1) # <class 'tuple'>
+print(tuple1)  # <class 'tuple'>
 
 a, b, c = tuple1
 print(a, b, c)
 
 
-print("Hello" in tuple1) #True
-print(not("hello" not in tuple1))
+print("Hello" in tuple1)  # True
+print(not ("hello" not in tuple1))
 
 x = (1, 2, 3, [4, 5])
 x[3][1] = 100
@@ -114,10 +123,10 @@ x[3][1] = 100
 print(x)
 
 tuple_a = (1, 2, 3, 4, 5)
-tuple_b = ( 'a', 'b', 'c' , 'd', 'e')
+tuple_b = ("a", "b", "c", "d", "e")
 
 zipped = zip(tuple_a, tuple_b)
-print(zipped) # ‹zip object at 0x10eb20b08>
+print(zipped)  # ‹zip object at 0x10eb20b08>
 
 result = tuple(zipped)
 print(result)
@@ -126,7 +135,7 @@ t1, t2 = zip(*result)
 print("t1: ", t1, "\nt2:", t2)
 
 
-a =(" John", "Charles", "Mike")
+a = (" John", "Charles", "Mike")
 b = ("Manager", "Supervisor", "Engineer")
 x = zip(a, b)
 
@@ -135,14 +144,14 @@ c = dict(x)
 print(c)
 
 
-print("-"*50)
+print("-" * 50)
 
 bike_owners = {
-    "James":"Ducati Monster 1200", 
-    "Jacob":"Ducati Scrambler 1100", 
-    "William":"BMW S 1000 RR", 
-    "Aiden":"Harley Davidson"
-    }
+    "James": "Ducati Monster 1200",
+    "Jacob": "Ducati Scrambler 1100",
+    "William": "BMW S 1000 RR",
+    "Aiden": "Harley Davidson",
+}
 
 print(bike_owners)
 print(bike_owners.keys())
@@ -150,15 +159,11 @@ print(bike_owners.values())
 
 for i in bike_owners:
     print(f" {i} : {bike_owners[i]}")
-    
-for i in bike_owners: print(i)
 
-int_dict = {
-    1 : "ONE",
-    2 : "TWO",
-    3 : "Three",
-    3 : "THREE"
-}
+for i in bike_owners:
+    print(i)
+
+int_dict = {1: "ONE", 2: "TWO", 3: "Three", 3: "THREE"}
 
 print("Aiden" in bike_owners)
 
@@ -167,31 +172,28 @@ bike_owners["Alex"] = "Lastochka"
 print(bike_owners)
 
 
-
 fruits = {
-    'Banana': (50, 60, 40, 55),
-    'Apple': (78, 86, 80, 60),
-    'Apricot': [40, 70, 30, 55]}
+    "Banana": (50, 60, 40, 55),
+    "Apple": (78, 86, 80, 60),
+    "Apricot": [40, 70, 30, 55],
+}
 
 print(fruits["Banana"][3])
 
 for i in fruits:
-    #print(i)
+    # print(i)
     for j in range(len(fruits[i])):
-        print(fruits[i][j], end = ", ", sep = "-")
+        print(fruits[i][j], end=", ", sep="-")
 print("\n")
-for i in fruits: 
+for i in fruits:
     fruits["Apricot"][3] = 0.5
-    
+
     for j in range(len(fruits[i])):
-        print(fruits[i][j], end = ", ")
+        print(fruits[i][j], end=", ")
 
 print("\n")
 
-fruit_qty_consumed = {
-    "Plum" : {'Fri': 70, 'Sat': 80},
-    'Apple': {'Fri': 90, 'Sat': 95}
-}
+fruit_qty_consumed = {"Plum": {"Fri": 70, "Sat": 80}, "Apple": {"Fri": 90, "Sat": 95}}
 
 print(fruit_qty_consumed["Plum"]["Fri"])
 
@@ -199,46 +201,43 @@ print(fruit_qty_consumed.pop("Plum"))
 
 print(fruit_qty_consumed.items())
 
-for i,j in fruits.items():
+for i, j in fruits.items():
     print(f"{i} : {j}")
-    
-    
+
+
 fruits = {
-    'Banana': (50, 60, 40, 55),
-    'Apple': (78, 86, 80, 60),
-    'Apricot': (40, 70, 30, 55),
-    'Orange': (70, 80, 60, 65),
-    'Avocado': (30, 50, 40, 45)}
+    "Banana": (50, 60, 40, 55),
+    "Apple": (78, 86, 80, 60),
+    "Apricot": (40, 70, 30, 55),
+    "Orange": (70, 80, 60, 65),
+    "Avocado": (30, 50, 40, 45),
+}
 
 # Remove item by key. Returns the value (i.e fruits.pop("Banana") -> (50, 60, 40, 55))
-fruits.pop("Banana") 
-fruits.popitem() # Removes random value from dict
+fruits.pop("Banana")
+fruits.popitem()  # Removes random value from dict
 
 # Sorting
-sorted(fruits, reverse = True) # Returns the list of sorted keys
-sorted(fruits.values()) # sort comparable values
+sorted(fruits, reverse=True)  # Returns the list of sorted keys
+sorted(fruits.values())  # sort comparable values
 
 # .items()
-fruits.items() # Returns a list of tuples (key, value)
-for i,j in fruits.items():
+fruits.items()  # Returns a list of tuples (key, value)
+for i, j in fruits.items():
     print(f"{i} : {j}")
 
-#Copy
+# Copy
 copy_fruits = fruits.copy()
 
 
-
-fruits = {
-    'Banana': (50, 60, 40, 55),
-    'Avocado': (30, 50, 40, 45)
-    }
+fruits = {"Banana": (50, 60, 40, 55), "Avocado": (30, 50, 40, 45)}
 
 # Concatenate dicts
-new_fruits = {"Cherry": (1, 2), "Banana": (1,2)}
+new_fruits = {"Cherry": (1, 2), "Banana": (1, 2)}
 fruits.update(new_fruits)
 print(fruits)
 
-print("-"*50)
+print("-" * 50)
 
 set_string = {"Emma", "Olivia", "Ava", "Mia"}
 print(set_string)
@@ -247,8 +246,8 @@ empty_set = set()
 print(f"{empty_set} of type {type(empty_set)}")
 
 
-a1 = {-1,0,1,2,3}
-a2 = {1,2,3,4}
+a1 = {-1, 0, 1, 2, 3}
+a2 = {1, 2, 3, 4}
 
 b1 = a1.intersection(a2)
 print(b1)
@@ -257,21 +256,20 @@ print(b1)
 print(a1.difference(a2))
 print(a2.symmetric_difference(a1))
 
-a3 = {2,3}
+a3 = {2, 3}
 
 print(a2.issuperset(a3))
 
 set1 = {"Abc", "Bcd", "Cde"}
 
-print(min(set1)) # Abc
-print(max(set1)) # Cde
+print(min(set1))  # Abc
+print(max(set1))  # Cde
 
 
-list1 = [[1,[2.0 , 2.1]],[3,4],[5,6]]
+list1 = [[1, [2.0, 2.1]], [3, 4], [5, 6]]
 dict1 = dict(list1)
 
-print(dict1) # {1: [2.0, 2.1], 3: 4, 5: 6}
-
+print(dict1)  # {1: [2.0, 2.1], 3: 4, 5: 6}
 
 
 list2 = list(dict1.items())
@@ -294,7 +292,6 @@ except:
     print("Something went wrong")
 
 
-
 import time, datetime
 
 # try:
@@ -312,9 +309,9 @@ print()
 try:
     file = open("non_existen_file", "r")
 except OSError:
-    print("OSError occured") # Superset of FileNotFoundError
+    print("OSError occured")  # Superset of FileNotFoundError
 except FileNotFoundError:
-    print("File does not exist") # Subset of OSError
+    print("File does not exist")  # Subset of OSError
 
 print()
 
@@ -328,15 +325,14 @@ finally:
 var = "Hello there"
 
 try:
-    with open('../TEST/Linux/test.py', "r") as file:
+    with open("../TEST/Linux/test.py", "r") as file:
         print(file.read())
 finally:
     print("Gracefully exiting the try-except-finally block")
 
 
-
 try:
-    file = open('../TEST/Linux/test.py', "r")
+    file = open("../TEST/Linux/test.py", "r")
     print(file.read())
 finally:
     print("Closing the file")
@@ -348,31 +344,39 @@ import errno, os
 
 for i in dir(errno):
     print(i)
+
+
 def check_file(filepath):
     if not os.path.isfile(filepath):
-        raise FileNotFoundError # Not recommended
+        raise FileNotFoundError  # Not recommended
+
 
 try:
-    check_file('invalid.path')
+    check_file("invalid.path")
 except FileNotFoundError as exception:
-    print('#1 Handled FileNotFoundError:', exception)
+    print("#1 Handled FileNotFoundError:", exception)
+
 
 def check_file_message(filepath):
     if not os.path.isfile(filepath):
-            raise FileNotFoundError('"%s" not found' % filepath)
+        raise FileNotFoundError('"%s" not found' % filepath)
+
 
 try:
-    check_file_message('invalid.path')
+    check_file_message("invalid.path")
 except FileNotFoundError as exception:
-    print('#2 Handled FileNotFoundError:', exception)
+    print("#2 Handled FileNotFoundError:", exception)
 
-def check_file_args(filename) :
+
+def check_file_args(filename):
     if not os.path.isfile(filename):
-        raise FileNotFoundError(errno.ENOENT, os. strerror (errno. ENOENT), 
-                                filename, 2, 'filename2')
+        raise FileNotFoundError(
+            errno.ENOENT, os.strerror(errno.ENOENT), filename, 2, "filename2"
+        )
+
 
 try:
-    check_file_args('invalid.path')
+    check_file_args("invalid.path")
 except FileNotFoundError as exception:
-    print('#3 Handled FileNotFoundError:', exception)
-    print('Arguments of exception:', exception. args)
+    print("#3 Handled FileNotFoundError:", exception)
+    print("Arguments of exception:", exception.args)
--- PCAP-31-03/test3.py
+++ PCAP-31-03/test3.py
@@ -1,5 +1,6 @@
 # -3
 import math
+
 x = 1.7
 print(-abs(math.floor(x) + math.ceil(x)))
 
@@ -15,23 +16,26 @@ print(-abs(math.floor(x) + math.ceil(x)))
 # ('list index out of range',)
 data = [261, 321]
 try:
-    print(data [-3])
+    print(data[-3])
 except Exception as exception:
-    print(exception. args)
+    print(exception.args)
 else:
     print(" ('success',) ")
-
 
 
 # -2
 def attic(x):
     assert x != 0
-    return 1/x
+    return 1 / x
+
+
 def floor(x):
     try:
         attic(x)
     except:
         raise
+
+
 try:
     x = attic(0)
 except RuntimeError:
@@ -40,36 +44,36 @@ except:
     x = -2
 else:
     x = -1
-print (x)
-
+print(x)
 
 
 # e == 2
-s = 'three'
+s = "three"
 e = 0
 try:
     e = int(s)  # Value Error
 except ArithmeticError:
     e = 1
-except: 
+except:
     e = 2
 else:
     e = 3
 print(e)
 
 
-
-
-
-# 0 
-header = 2 *'+-' + '+' # +-+-+
+# 0
+header = 2 * "+-" + "+"  # +-+-+
 plus = 0
-for x in header: # The for loop iterates over each character in header. In this case, it iterates over each character in '+-+-+'.
-    if not x in header: # Inside the loop, there is an if statement: if not x in header:. This condition checks if the current character x is not in the header. However, since x is always a character from header (due to the loop iterating over header itself), this condition will never be true.
+for (
+    x
+) in (
+    header
+):  # The for loop iterates over each character in header. In this case, it iterates over each character in '+-+-+'.
+    if (
+        not x in header
+    ):  # Inside the loop, there is an if statement: if not x in header:. This condition checks if the current character x is not in the header. However, since x is always a character from header (due to the loop iterating over header itself), this condition will never be true.
         plus += 1
-print (plus)
-
-
+print(plus)
 
 
 # TRUE
@@ -83,37 +87,39 @@ print (plus)
 # True False
 class Cat:
     species = 1
+
     def get_species(self):
-        return 'kitty'
-    
-class Tiger (Cat):
+        return "kitty"
+
+
+class Tiger(Cat):
     def get_species(self):
-        return 'tiggy'
-    
+        return "tiggy"
+
+
 def set_species(self):
     pass
+
 
 creature = Tiger()
 print(hasattr(creature, "Species"), hasattr(Cat, "set species"))
 
 
-
-
-
-#Increment __b with fucntions:
+# Increment __b with fucntions:
 # object._A__b += 1
 # A._A__B += 1
 class A:
-    __b= 1
+    __b = 1
+
     def __init__(self):
         self.c = 1
-def __action(self) :
+
+
+def __action(self):
     pass
 
+
 object = A()
-
-
-
 
 
 # TRUE
@@ -124,90 +130,121 @@ object = A()
 # start.my_ID ==-2
 class Control:
     my_ID = 1
-def say (self) :
+
+
+def say(self):
     return self.my_ID
+
+
 class Button(Control):
     my_ID = 2
+
+
 class Radio(Button):
     def say(self):
-        return -self. my_ID
+        return -self.my_ID
+
+
 selection = Radio()
 element = Control()
 start = Button()
 
 
-
-
 #
 class Diamond:
     pass
+
+
 class Adamant(Diamond):
     pass
+
+
 class Gem(Diamond):
     pass
+
 
 # No exception:
 class Jewel(Adamant, Diamond):
     pass
-class Jewel(Adamant,Gem):
+
+
+class Jewel(Adamant, Gem):
     pass
+
+
 # Raise exception
-class Jewel(Diamond, Diamond): # Jewel is attempting to inherit from Diamond twice. This will raise an exception because in Python, multiple inheritance doesn't allow duplicate base classes.
-    pass
-class Jewel(Diamond, Adamant): #  Similarly, attempting to inherit from Diamond and Adamant (which is already a subclass of Diamond) will raise an exception due to duplicate base classes.
+class Jewel(
+    Diamond, Diamond
+):  # Jewel is attempting to inherit from Diamond twice. This will raise an exception because in Python, multiple inheritance doesn't allow duplicate base classes.
     pass
 
 
+class Jewel(
+    Diamond, Adamant
+):  #  Similarly, attempting to inherit from Diamond and Adamant (which is already a subclass of Diamond) will raise an exception due to duplicate base classes.
+    pass
 
 
 # True no exception
-#len(edition.__dict__ ! = len (Volume.__dict__)
+# len(edition.__dict__ ! = len (Volume.__dict__)
 # Volume.__dict__['chapter'] != None
 
 # Fail or False
 # 'paragraph' in Volume.__dict__
 # paragraph' in edition.__dict__
 
+
 class Volume:
     chapter = 1
+
     def __init__(self, paragraph):
         self.paragraph = paragraph
         Volume.chapter += 1
-    def remove(self) :
+
+    def remove(self):
         del self.paragraph
+
+
 edition = Volume(1)
 edition.remove()
-
-
 
 
 # Soution below
 class Un:
     value = "Ein"
-    def say (self):
+
+    def say(self):
         return self.value.upper()
+
+
 class Deux(Un):
     value = "Zwei"
+
+
 class Troi(Un):
     def say(self):
         return self.value.lower()
-class Quatre (Troi, Deux):
+
+
+class Quatre(Troi, Deux):
     pass
+
+
 d = Quatre()
 b = Deux()
 
-#Solution
-print(isinstance(d, Un)) # True
-print(Troi in Quatre.__bases__) # True
+# Solution
+print(isinstance(d, Un))  # True
+print(Troi in Quatre.__bases__)  # True
 # print(d.say("ZWEI")) # TypeError: say() takes 1 positional argument but 2 were given
-print(d.say() == "ZWEI") # False
-#print(q.value() =="uno") # NameError:name ' q'is not defined
+print(d.say() == "ZWEI")  # False
+# print(q.value() =="uno") # NameError:name ' q'is not defined
 
 
-#Valid Lambda
-lambda a,b: a if a < b else b
-lambda a,b: True
-#Invalid lambda
+# Valid Lambda
+lambda a, b: a if a < b else b
+lambda a, b: True
+# Invalid lambda
 # lambda a, b = a if a < b else b
 # lambda (a, b) : return a if a < b else b
 
@@ -218,19 +255,18 @@ lambda a,b: True
 
 # Prins 1 from array below
 # [0, 1, 2, 4]
-source = [1,2, 4, 8,16]
-target = [x//2 for x in source if x < 10]
+source = [1, 2, 4, 8, 16]
+target = [x // 2 for x in source if x < 10]
 print(target[1])
 
 
 # 1
 pairs = [[2, 1], [-2, -1]]
-new_pairs = map(lambda p: sorted(p) , pairs)
+new_pairs = map(lambda p: sorted(p), pairs)
 print(list(new_pairs)[0][0])
 
 
-
-#TRUE
+# TRUE
 # There are three pre-opened file streams.
 # The input() function reads data from the stdin stream.
 # False
@@ -238,29 +274,29 @@ print(list(new_pairs)[0][0])
 # The first argument of the open() function is an integer value.
 
 
-
-
-
 # Error will occure due to b(b)
 def f(a, b):
     return b(b)
+
+
 print(f(lambda x: x + 1, 0))
 
 
-
-
-#True
+# True
 def boolean(op):
     return op(False, True)
-print(boolean(lambda x,y: x if x else y))
 
 
+print(boolean(lambda x, y: x if x else y))
 
 
-# 8 
+# 8
 def power(a):
     def internal(x):
-        return x ** a;
+        return x**a
+
     return internal
-cube = power(3) # internal() with a==3
-print(cube(2)) # 2 ** 3 == 8
+
+
+cube = power(3)  # internal() with a==3
+print(cube(2))  # 2 ** 3 == 8
