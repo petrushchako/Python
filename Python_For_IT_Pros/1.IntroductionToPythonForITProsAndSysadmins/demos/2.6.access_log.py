78e30cf5e8e6a201bfaae8c1d23ca8008c4c5179
Oleksandr Petrushchak <petrushchako@gmail.com>
2025-08-29 20:59:53 +0100

    Add log parsing logic

---
+++ Python_For_IT_Pros/1.IntroductionToPythonForITProsAndSysadmins/demos/2.6.access_log.py
@@ -0,0 +1,28 @@
+import re
+from collections import Counter
+
+log_file_path = "access.log"
+
+# Regular expression
+# \d+  - one or more digits (0–9)
+# \.   - a literal dot (.) The backslash is needed because . by itself means "any character" in regex
+# Example match: 192.168.0.1
+ip_pattern = r"\d+\.\d+\.\d+\.\d+"
+
+
+def analyze_log(log_file_path):
+    with open(log_file_path) as file:
+        log_lines = file.readlines()
+
+    ip_addresses = re.findall(ip_pattern, "".join(log_lines))
+    unique_ips = set(ip_addresses)
+    num_requests = len(log_lines)
+
+    url_counter = Counter()
+    for line in log_lines:
+        match = re.search(r'"GET (.*?) HTTP', line)
+        if match:
+            url = match.group(1)
+            url_counter[url] += 1
+
+    return num_requests, unique_ips, url_counter.most_common(3)
