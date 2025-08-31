e659a0ae77f400b63e75152bd0e69b0a955714cc
Alex Petrushchak <petrushchako@gmail.com>
2025-08-31 22:43:29 +0100

    Add backup menu option

--- Python_For_IT_Pros/1.IntroductionToPythonForITProsAndSysadmins/demos/2.7.backup_restore.py
+++ Python_For_IT_Pros/1.IntroductionToPythonForITProsAndSysadmins/demos/2.7.backup_restore.py
@@ -24,3 +24,24 @@ def recover_backup(backup_path, destination):
     except Exception as e:
         print(f"Backup Failed: {e}")
 
+
+if __name__ == "__main__":
+    print("Choose and option\n\t(1) Backup\n\t(2) Restore")
+
+    choice = input("Enter your choice (1 or 2): ")
+    if choice == "1":
+        backup_directory_contents(source_directory, backup_directory)
+    elif choice == "2":
+        backup_list = os.listdir(backup_directory)
+        print("Available backups")
+        for idx, backup in enumerate(backup_list, start=1):
+            print(f"{idx}. {backup}")
+        
+        backup_index = int(input("Enter the backup index to recover: "))
+        if 0 <= backup_index < len(backup_list):
+            select_backup = os.path.join(backup_directory, backup_list[backup_index])
+            recover_backup(select_backup, source_directory)
+        else:
+            print("Invalid backup index")
+    else:
+        print("Invalid option!\nHINT: use options 1 or 2")
