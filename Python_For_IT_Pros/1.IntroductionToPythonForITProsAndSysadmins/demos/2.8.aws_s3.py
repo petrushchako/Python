0d1b8d7f4b4bfbeed8116ee227dc97a0e4a74bd9
Alex Petrushchak <petrushchako@gmail.com>
2025-09-01 23:06:08 +0100

    Add create bucket logic

--- Python_For_IT_Pros/1.IntroductionToPythonForITProsAndSysadmins/demos/2.8.aws_s3.py
+++ Python_For_IT_Pros/1.IntroductionToPythonForITProsAndSysadmins/demos/2.8.aws_s3.py
@@ -4,9 +4,26 @@ aws_access_key = ""
 aws_secret_key = ""
 aws_region = "eu-west-1"
 
+bucket_name = "super_unique_bucket_12345"
+
+
 s3_client = boto3.client(
     "s3",
     aws_access_key_id=aws_access_key,
     aws_secret_access_key=aws_secret_key,
-    region_name= aws_region
+    region_name= aws_region 
 )
+
+
+def get_location_constraint(region_name):
+    if region_name == "us-west-1":
+        return ""
+    else:
+        return region_name
+    
+
+def create_s3_bucket(bucket_name):
+    response = s3_client.create_bucket(Bucket=bucket_name)
+    print(f"S3 Bucket '{bucket_name}' created successfully")
+
+
