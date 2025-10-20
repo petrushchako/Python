70200fd488d2e8e34ccbf10bffb903c86fadfa19
Oleksandr Petrushchak <petrushchako@gmail.com>
2025-10-20 23:02:19 +0100

    Update requirements.txt

---
+++ Python_For_IT_Pros/2.PythonForLinuxSystemAdministration/demos/serverhealth.py
@@ -0,0 +1,13 @@
+# Import the required libraries
+# Ensure psutil is installed before running this script.
+# To install run pip install psutil
+import psutil
+import os
+import platform
+import time
+import socket
+from subprocess import call
+# Ensure prettytable is installed before running this script.
+# To install run pip install prettytable
+
+from prettytable import PrettyTable
