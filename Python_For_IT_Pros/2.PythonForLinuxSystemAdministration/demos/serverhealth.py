04436f39eb7ba6f6b2eff347e1c8661a26b8b911
Oleksandr Petrushchak <petrushchako@gmail.com>
2025-10-21 22:49:34 +0100

    Add monitoring system information printout

--- Python_For_IT_Pros/2.PythonForLinuxSystemAdministration/demos/serverhealth.py
+++ Python_For_IT_Pros/2.PythonForLinuxSystemAdministration/demos/serverhealth.py
@@ -11,3 +11,28 @@ from subprocess import call
 # To install run pip install prettytable
 
 from prettytable import PrettyTable
+
+# Run infinite loop to check server health
+
+while True:
+    # Clear the screen using a bash command
+    call('clear')
+
+    print("="*20, " Server Health", "="*20)
+
+    # PrettyTable is used to print data in the terminal
+    # t = PrettyTable(<list of headings>)
+    # t.add_row(<list of cells in the row>)
+
+    # Server OS info
+    print("\n")
+    print("-------Operating System Information------")
+    print("Operating System Type:", os.name)
+    print("\nOperating Sysyem:", platform.system())
+    print("\nOperating System Version:", platform.release())
+    print("\nSystem Bit Architecture:", platform.architecture())
+    print("\nComputer Name:", platform.node())
+    hostname = platform.node()
+    IPAddr = socket.gethostbyname(hostname)
+    print("\nCompiter IP Address:", IPAddr)
+    print("\n")
