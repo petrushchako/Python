6d2fd3ebc04fd97ea3f86481096c90d693ec7f33
Oleksandr Petrushchak <60898768+petrushchako@users.noreply.github.com>
2026-04-09 22:58:48 +0100

    add hard-1 median number problem

---
+++ leetcode/hard/4_median_of_two_sorted_arrays.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        merged = sorted(nums1 + nums2)
+        n = len(merged)
+        
+        if n % 2 == 0:
+            return float((merged[n//2] + merged[n//2-1])/2)
+        else:
+            return float(merged[n//2])
